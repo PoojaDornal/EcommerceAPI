f21036f197e090416d23a96456a4724a6471ac2a
PoojaDornal <poojadornal@gmail.com>
2023-09-19 22:53:22 +0530

    added mongoatlas connection

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const port=8006;
 const app = express();
 
 // Establish connection to the MongoDB database
+// mongoose.connect("mongodb://127.0.0.1:27017/product", { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log("Database connection successful");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
 mongoose.connect("mongodb+srv://poojadornal:pooja123@cluster0.cwaxtln.mongodb.net/EcommerceApi?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Database connection successful");
